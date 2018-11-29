a81e7ba6e477999b628e36ff86816f893ee40bc7
Grant Quick <32093575+GrantQuick@users.noreply.github.com>
2018-11-29 12:06:04 +0000

    Add files via upload

---
+++ Connect-SkyApi.ps1
@@ -0,0 +1,68 @@
+Function Connect-SkyApi {
+    [CmdletBinding()]
+    param([Switch]$force)
+    
+    Import-Module 'SkyApi' -Force
+
+    # Database Parameters
+    $authorize_uri = "https://oauth2.sky.blackbaud.com/authorization"
+    $redirect_uri = "http://localhost/5000"
+
+    $config = Get-Content ".\Config.json" | ConvertFrom-Json
+    $key_dir = ($config | Select-Object -Property "key_dir").key_dir
+    $client_id = ($config | Select-Object -Property "client_id").client_id
+    $client_secret = ($config | Select-Object -Property "client_secret").client_secret
+
+    [Reflection.Assembly]::LoadWithPartialName("System.Web") | Out-Null
+
+    # Build authorisation URI
+    $strUri = $authorize_uri +
+        "?client_id=$client_id" +
+        "&redirect_uri=" + [System.Web.HttpUtility]::UrlEncode($redirect_uri) +
+        '&response_type=code&state=state'
+
+    Function Get-NewToken
+    {
+        [CmdletBinding()]
+        param($fileLocation)
+    
+        $authOutput = Show-OAuthWindow -URL $strUri
+    
+        # Get auth token
+        $Authorization = Get-SkyApiAuthToken 'authorization_code' $client_id $redirect_uri $client_secret $authOutput["code"]
+        
+        # Swap token for a refresh token
+        $Authorization = Get-RefreshToken 'refresh_token' $client_id $redirect_uri $client_secret $authorization.refresh_token
+    
+        # Save credentials to file
+        $Authorization | Select-Object access_token, refresh_token | ConvertTo-Json | Out-File -FilePath $fileLocation -Force
+        
+    }
+    
+    # If key file does not exist
+    if (-not (Test-Path $key_dir))
+    {
+        Get-NewToken $key_dir
+    }
+    
+    # Check if refresh token is nearing expiry, and if so get a new one
+    $lastWrite = (get-item $key_dir).LastWriteTime
+    $minTimespan = new-timespan -days 30
+    $maxTimespan = new-timespan -days 60
+    
+    # Token is older than 30 days and but younger than 60
+    if ((((get-date) - $lastWrite) -gt $minTimespan) -and (((get-date) - $lastWrite) -lt $maxTimespan))  {
+        Write-Host "older"
+        $myAuth = Get-Content $key_dir | ConvertFrom-Json
+        $Authorization = Get-RefreshToken 'refresh_token' $client_id $redirect_uri $client_secret $($myAuth.refresh_token)
+        $Authorization | Select-Object access_token, refresh_token | ConvertTo-Json | Out-File -FilePath $key_dir -Force
+    } 
+    
+    # If token has expired
+    if (((get-date) - $lastWrite) -gt $maxTimespan) {
+        Get-NewToken $key_dir
+    }
+}
+
+
+
---
+++ Patch-Education.ps1
@@ -0,0 +1,121 @@
+Function Patch-Education
+{
+    [cmdletbinding()]
+    param(
+        [parameter(
+            #Position=0,
+            Mandatory=$true, 
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string[]]$ID,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$Campus,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$Class_Of,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$Class_Of_Degree,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$Degree,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$Department,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$Faculty,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$Known_Name,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$Registration_Number,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$School,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$Social_Organization,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$Status,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$Subject_Of_Study,
+        [parameter(
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+            )
+        ][string]$Type
+    )
+    Begin{ 
+        
+        # Get necessary items from config file
+        $config = Get-Content ".\Config.json" | ConvertFrom-Json
+        $api_subscription_key = ($config | Select-Object -Property "api_subscription_key").api_subscription_key
+        $key_dir = ($config | Select-Object -Property "key_dir").key_dir
+        $myAuth = Get-Content $key_dir | ConvertFrom-Json
+
+        Function Patch-Education
+        {
+            [CmdletBinding()]
+            param($uid, $updateProperties)
+
+            $startUrl = 'https://api.sky.blackbaud.com/constituent/v1/educations/'
+
+            $fullUri = $startUrl + $uid
+
+            $apiCallResult =
+            Invoke-RestMethod   -Method Patch `
+                                -ContentType application/json `
+                                -Headers @{
+                                        # 'Authorization' = ("Bearer "+ $Authorization.access_token)
+                                        'Authorization' = ("Bearer "+ $($myAuth.access_token))
+                                        'bb-api-subscription-key' = ($api_subscription_key)} `
+                                -Uri $fullUri `
+                                -Body $updateProperties
+            $apiCallResult
+        }
+   
+        # Create JSON for supplied parameters
+        $parms = $PSBoundParameters
+        $parms.Remove('ID') | Out-Null
+        $parmsJson = $parms | ConvertTo-Json
+        
+    }
+    
+    Process{ 
+        # Update multiple IDs with the same data
+        $ID | ForEach-Object {
+            Patch-Education $_ $parmsJson
+        }
+    }
+    End{}
+}
